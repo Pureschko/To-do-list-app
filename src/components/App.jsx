2c628d8ef4da8a44bbe9edbff7f3bb89a334fa91
Pureschko <inna.shekina@gmail.com>
2024-09-16 21:55:21 +0200

    Finished the task

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import AddToDo from "./AddToDo";
+import FilterComponent from "./FilterComponent";
+import ToDoList from "./ToDoList";
+
+
+const App = () => {
+  const todos = [
+    {
+      id: 1,
+      text: 'Learn React',
+      completed: false
+    },
+    {
+      id: 2,
+      text: 'Learn Tailwind CSS',
+      completed: true
+    },
+    {
+      id: 3,
+      text: 'Learn Node.js',
+      completed: false
+    },
+    {
+      id: 4,
+      text: 'Learn Express.js',
+      completed: false
+    }
+  ];
+ 
+  return (
+    <div className='container mx-auto p-4'>
+      <AddToDo />
+      <FilterComponent />
+      <ToDoList todos={todos} />
+    </div>
+  );
+};
+ 
+export default App;
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,10 @@
+const ToDoItem = ({ todo }) => {
+    return (
+      <li className='flex items-center mb-2'>
+        <input type='checkbox' defaultChecked={todo.completed} className='mr-2' />
+        {todo.text}
+      </li>
+    );
+  };
+   
+  export default ToDoItem;
