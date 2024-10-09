e06424c030a9d3a8ac1d9dac9992bd13164cb3d6
Pureschko <inna.shekina@gmail.com>
2024-10-09 14:35:58 +0200

    1st commit

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,10 +1,15 @@
-const ToDoItem = ({ todo }) => {
-    return (
-      <li className='flex items-center mb-2'>
-        <input type='checkbox' defaultChecked={todo.completed} className='mr-2' />
-        {todo.text}
-      </li>
-    );
-  };
-   
-  export default ToDoItem;
+const ToDoItem = ({ todo, toggleTodo }) => {
+  return (
+    <li className='flex items-center mb-2'>
+      <input
+        type='checkbox'
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+        className='mr-2'
+      />
+      {todo.text}
+    </li>
+  );
+};
+ 
+export default ToDoItem;
